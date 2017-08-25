fd9a1598764b5b8a75d91bdc031be3e8420c3241
Zhou Wang <zhouwang@microsoft.com>
2017-08-25 10:04:38 +0200

    add new CNTKLibraryCPPEvalExamples

--- Examples/Evaluation/CNTKLibraryCPPEvalGPUExamples/CNTKLibraryCPPEvalGPUExamples.cpp
+++ Examples/Evaluation/CNTKLibraryCPPEvalGPUExamples/CNTKLibraryCPPEvalGPUExamples.cpp
@@ -2,7 +2,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
-// CNTKLibraryCPPEvalGPUExamples.cpp : Sample application shows how to evaluate a model using CNTK V2 API.
+// CNTKLibraryCPPEvalGPUExamples.cpp: Model evaluation using CNTK V2 C++ API on GPU devices.
 //
 
 #include <stdio.h>
